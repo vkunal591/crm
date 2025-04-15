53ae7d16614852a0d3a8a551f447cf74121dc57a
Kunal Verma <vkunal591@gmail.com>
2025-04-15 15:48:22 +0530

    remove copuright

--- src/components/common/Wrapper.tsx
+++ src/components/common/Wrapper.tsx
@@ -10,14 +10,13 @@ const Wrapper = ({
   return (
     <div className="bg-infobg pt-[70px] ml-[17%]">
       <div className="p-4 pt-2 min-h-screen">{children}</div>
-      <p className="flex text-sm p-5 gap-1 text-gray-500 bg-whiteBg justify-center items-center">
+      <p className="flex text-sm p-5 gap-1 text-gray-500 bg-whiteBg justify-center items-center hidden">
         Copywrite © {new Date().getFullYear()}
-        <strong className="text-gray-800"> SkyField</strong>. Designed with ❤️
+        <strong className="text-gray-800"> Silk India</strong>. Designed with ❤️
         by
         <strong className="underline text-gray-800">
-          <Link href={"https://www.volvrit.com"} target="_blank">
-            {" "}
-            Volvrit
+          <Link href={"https://www.example.com"} target="_blank">
+            {"example"}
           </Link>
         </strong>
         . All Rights Reserved
